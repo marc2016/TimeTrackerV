df2b55c8ca285c345d8bc7106e360c8c7615ee48
Marc Lammers <marc@lammers.dev>
2025-03-16 12:22:48 +0100

    Added hasTaskChanged for tasks

---
+++ src/helper/TaskHelper.ts
@@ -0,0 +1,5 @@
+function hasTaskChanged(task1: Task, task2: Task): boolean {
+  return task1.name !== task2.name || task1.description !== task2.description || task1.done !== task2.done
+}
+
+export { hasTaskChanged };
